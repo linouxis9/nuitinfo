0378d8ebdcd9dffd6001699bdbe2b50ec3d9771c
Vincent Lefebvre <lefebvvi@ltsp3.iut-valence.fr>
2018-12-06 23:48:07 +0100

    speed

--- JS/geolocalisation.js
+++ JS/geolocalisation.js
@@ -1,11 +1,17 @@
 
-var intervalID = window.setInterval(showPosition, 1000);
 
-var intervalID = window.setInterval(hasMoved, 10000);
+  var dist;
+  var vraiDist;
+function geoLoc(){
+var intervalID = window.setInterval(showPosition, 200);
+
+//var intervalID = window.setInterval(hasMoved, 1500);
+}
 
 function showPosition(){
   if(navigator.geolocation)
-    navigator.geolocation.watchPosition(maPosition);
+   navigator.geolocation.watchPosition(maPosition);
+
 }
 
 function maPosition(position){
@@ -20,7 +26,19 @@ function maPosition(position){
       document.getElementById("lastAltitude").innerHTML = document.getElementById("currentAltitude").innerText;
       document.getElementById("lastLongitude").innerHTML = document.getElementById("currentLongitude").innerText;
       document.getElementById("lastLatitude").innerHTML = document.getElementById("currentLatitude").innerText;
+      var lastLongitude= 1*document.getElementById("currentLongitude").innerText ;
+     var  lastLatitude=1*document.getElementById("lastLatitude").innerText;
+      var  currentLatitude=1*document.getElementById("currentLatitude").innerText;
+      var  currentLongitude=1*document.getElementById("currentLongitude").innerText;
+      console.log("ye s");
+       dist =Math.cos(currentLongitude-lastLongitude);
 
+      console.log("no");
+
+
+     vraiDist=Math.acos((Math.cos((Math.sin(lastLatitude)*Math.sin(currentLatitude))+(Math.cos(lastLatitude)*Math.cos(currentLatitude)*dist))))
+
+     document.getElementById("distance").innerText = vraiDist;
     }
 
     document.getElementById("currentAltitude").innerHTML = altitude;
@@ -34,6 +52,9 @@ function maPosition(position){
 }
 
 function hasMoved(){
-if(document.getElementById("lastAltitude").innerText== document.getElementById("currentAltitude").innerText)
-  alert(" Vous ne bougez plus !")
+
+
+
+
+//  alert(" Vous ne bougez plus !")
 }
